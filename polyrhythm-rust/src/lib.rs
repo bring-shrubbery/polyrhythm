6f4e44c3113ef4b879d9af9e53618b5a60c817e8
Antoni Silvestrovič <antoni.silvestrovic@gmail.com>
2022-04-09 18:40:49 +0300

    Added more tests

--- polyrhythm-rust/src/lib.rs
+++ polyrhythm-rust/src/lib.rs
@@ -22,7 +22,7 @@ pub fn get_polyrhythm(beats: &Vec<usize>) -> Option<Vec<Vec<usize>>> {
 }
 
 #[cfg(test)]
-mod tests {
+mod polyrhythm_tests {
   use crate::get_polyrhythm;
   #[test]
   fn basic_3_2_polyrhythm() {
@@ -30,10 +30,27 @@ mod tests {
     let result = get_polyrhythm(&beats);
 
     match result {
-      Some(res) => {
-        assert_eq!(res, vec![vec![1, 0, 1, 0, 1, 0], vec![1, 0, 0, 1, 0, 0]]);
-      }
-      None => panic!("Must return a value here!"),
+      Some(res) => assert_eq!(res, vec![vec![1, 0, 1, 0, 1, 0], vec![1, 0, 0, 1, 0, 0]]),
+      None => panic!("Must return 2D vector here."),
     }
+  }
+
+  #[test]
+  fn basic_2_3_polyrhythm() {
+    let beats = vec![2, 3];
+    let result = get_polyrhythm(&beats);
+
+    match result {
+      Some(res) => assert_eq!(res, vec![vec![1, 0, 0, 1, 0, 0], vec![1, 0, 1, 0, 1, 0]]),
+      None => panic!("Must return 2D vector here."),
+    }
+  }
+
+  #[test]
+  fn non_valid_beats() {
+    let beats = vec![];
+    let result = get_polyrhythm(&beats);
+
+    assert_eq!(result, None);
   }
 }
